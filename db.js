e9d52cc01a49e2244bda9ca5419d471490467e1e
Lukasz8791 <doktor8on@gmail.com>
2024-04-28 12:08:38 +0200

    poprawki 2

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.pfqom1s.mongodb.net/?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
